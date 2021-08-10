16ff2677e7c6bc2ad1383c971c4416cdf55ce650
Sarmouts <saradis.m06@gmail.com>
2021-08-10 16:48:18 +0300

    Add project files.

---
+++ exercise/exercise.cpp
@@ -0,0 +1,15 @@
+// exercise.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+int main()
+{
+	char a, b;
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		cout << a << endl;
+	}
+	cout << a;
+
+}
